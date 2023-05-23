f0f940367832fb07a989d92a14ec56073ff19253
yuvi1987 <134362023+yuvi1987@users.noreply.github.com>
2023-05-23 12:50:00 +0530

    Add files via upload

--- FirstAssignment.java
+++ FirstAssignment.java
@@ -1,31 +1,38 @@
 package Package;
 
-class FirstAssignment {
+public class FirstAssignment {
 	
 	static int x = 50;
 	static int y = 30;
 	int a;
-	int b;
+
 	
 	public void addition() {
 		a = x+y;
 		System.out.println("The Added Value is: " + a);
 	}
 	
-	public void subtraction() {
-		b = x-y;
-		System.out.println("The Subracted Value is: " + b);
-	}
+
 	
 	public static void main(String[] args) {
 
 		FirstAssignment ins = new FirstAssignment();
 		ins.addition();
 		
-		FirstAssignment ins2 = new FirstAssignment();
+		SubtractClass ins2 = new SubtractClass();
 		ins2.subtraction();
 		
 	}
 
+}
+
+class SubtractClass{
+	static int x = 50;
+	static int y = 30;
+	int b;
+	public void subtraction() {
+		b = x-y;
+		System.out.println("The Subracted Value is: " + b);
+	}
 }
 
